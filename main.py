cf6397fc6a9e654ed3570ff5527a3894c506317f
danielavillamar <46539048+danielavillamar@users.noreply.github.com>
2023-02-26 19:18:59 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 # import generate_AFN_from_re 
 from Functions import *
 
-# ask for the regular expression
-regular_expression = input("Ingresar la expresión regular: ")
+try:
+  # ask for the regular expression
+  regular_expression = input("Enter the regular expression: ")
 
-# generate the AFN
-thompson_algorithm(regular_expression)
+  # generate the AFN
+  thompson_algorithm(regular_expression)
+  
+ except:
+    print("Expresión regular no válida")
 
 
 
